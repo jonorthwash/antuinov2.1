1949b9df0107a04c36d558331eecf300b86f03b6
Jonathan Washington <jonathan.n.washington@gmail.com>
2025-05-17 15:31:29 -0400

    better version of script

--- demo/analyseAntuino.py
+++ demo/analyseAntuino.py
@@ -3,6 +3,7 @@
 import os
 from datetime import datetime
 import AntuinoTerm
+import argparse
 
 bands = {
     '80m': ["3500000", "4000000", "10000"],
@@ -18,13 +19,35 @@ bands = {
     '00.7m': ["420000000", "450000000", "100000"]
 }
 
-time = datetime.now().strftime('%Y%m%d%H%M%S')
+def main(bands, outdir='./logs/'):
+    time = datetime.now().strftime('%Y%m%d%H%M%S')
 
-for band in bands:
-    print(band)
-    (frequencies, swrs) = AntuinoTerm.main(bands[band][0], bands[band][1], bands[band][2])
+    for band in bands:
+        print(band)
+        (frequencies, swrs) = AntuinoTerm.main(bands[band][0], bands[band][1], bands[band][2])
 
-    with open(os.path.join('./', 'logs/', time+'-'+band), 'w') as logfile:
-        logfile.write("frequency\tswr\n")
-        for (freq, swr) in zip(frequencies, swrs):
-            logfile.write(str(freq)+"\t"+str(swr)+"\n")
+        with open(os.path.join(outdir, time+'-'+band), 'w') as logfile:
+            logfile.write("frequency\tswr\n")
+            for (freq, swr) in zip(frequencies, swrs):
+                logfile.write(str(freq)+"\t"+str(swr)+"\n")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='analyse SWR using Antuino for specified bands')
+    parser.add_argument('-l', '--logdir', type=str,
+        help='directory to store raw data', default="./logs")
+    parser.add_argument('-b', '--bands', type=str,
+        help='bands, comma-separated, e.g. "06m,02m,00.7m"', default=None)
+
+    args = parser.parse_args()
+
+    if args.bands:
+        testBands = {}
+        for band in args.bands.split(","):
+            print(args.bands, band)
+            testBands[band] = bands[band]
+    else:
+        testBands = bands
+
+    main(testBands, outdir=args.logdir)
+
+
